eb08c370551052b04bd6ff8c355b3c36a984dc24
Marszal <gs.marszalek@gmail.com>
2015-03-30 21:15:06 +0200

    Removed unnecessary casting

--- src/main/java/gs/marshall/javafxstarter/main/StarterApplication.java
+++ src/main/java/gs/marshall/javafxstarter/main/StarterApplication.java
@@ -18,7 +18,7 @@ public class StarterApplication extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("Java FX Starter");
-    Pane pane = (Pane) FXMLLoader.load(getClass().getResource("starter-application.fxml"));
+    Pane pane = FXMLLoader.load(getClass().getResource("starter-application.fxml"));
     Scene scene = new Scene(pane);
     primaryStage.setScene(scene);
     
