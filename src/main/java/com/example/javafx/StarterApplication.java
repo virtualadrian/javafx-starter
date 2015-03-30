0d6397be810ba9e74eb4681db17182fd4ee389ab
Marszal <gs.marszalek@gmail.com>
2015-03-30 21:31:11 +0200

    Changed name of main window

--- src/main/java/com/example/javafx/StarterApplication.java
+++ src/main/java/com/example/javafx/StarterApplication.java
@@ -11,18 +11,18 @@ import javafx.stage.Stage;
  */
 public class StarterApplication extends Application {
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    primaryStage.setTitle("Java FX Starter");
-    Pane pane = FXMLLoader.load(getClass().getResource("starter-application.fxml"));
-    Scene scene = new Scene(pane);
-    primaryStage.setScene(scene);
-    
-    primaryStage.show();
-  }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Java FX Starter Application");
+        Pane pane = FXMLLoader.load(getClass().getResource("starter-application.fxml"));
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
 
 }
