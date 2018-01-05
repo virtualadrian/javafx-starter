44caa350ce29a067c1595fe0759913aef4c198c1
Marszal <gs.marszalek@gmail.com>
2018-01-05 20:04:55 +0100

    Updated formatting in StarterController

--- src/main/java/com/example/javafx/StarterController.java
+++ src/main/java/com/example/javafx/StarterController.java
@@ -1,32 +1,32 @@
 package com.example.javafx;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Controller for starter-application.fxml file.
  */
 public class StarterController implements Initializable {
 
-  @FXML
-  private Button helloButton;
-  @FXML
-  private Label helloLabel;
-  
-  @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
-    System.out.println("StarterController is initialized.");
-  }
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Label helloLabel;
 
-  @FXML
-  protected void handleHelloButtonAction(ActionEvent event) {
-    helloLabel.setText("Hello JavaFX!");
-  }
-  
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("StarterController is initialized.");
+    }
+
+    @FXML
+    protected void handleHelloButtonAction(ActionEvent event) {
+        helloLabel.setText("Hello JavaFX!");
+    }
+
 }
